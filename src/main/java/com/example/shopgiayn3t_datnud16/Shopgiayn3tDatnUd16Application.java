81d73c11d4d9927aac035c162630ff8ec2023ea6
thienndph18157 <thienndph18157@fpt.edu.vn>
2023-01-31 22:16:43 +0700

    no message

---
+++ src/main/java/com/example/shopgiayn3t_datnud16/Shopgiayn3tDatnUd16Application.java
@@ -0,0 +1,13 @@
+package com.example.shopgiayn3t_datnud16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shopgiayn3tDatnUd16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shopgiayn3tDatnUd16Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shopgiayn3t_datnud16/Shopgiayn3tDatnUd16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopgiayn3t_datnud16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shopgiayn3tDatnUd16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
